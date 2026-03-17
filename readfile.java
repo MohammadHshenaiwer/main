8eb0f73ca2bc33fef3d388d2206076d13936bbbd
m7mdhani <mhmd.h.shenaiwer@gmail.com>
2026-03-17 10:45:32 +0300

    :main

---
+++ readfile.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class readfile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter NAME or GRADE");
+        String search = scan.nextLine();
+        try (BufferedReader input = new BufferedReader(new FileReader("writeinfile.txt"));) {
+            String line;
+            while ((line = input.readLine()) != null) {
+                String parts[] = line.split(",");
+                if (search.equalsIgnoreCase("Name"))
+                    System.out.println("Name: " + parts[0]);
+                else if (search.equalsIgnoreCase("Grade"))
+                    System.out.println("Grade: " + parts[1]);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
